421de71a521ab07e942ae46a8f0a8f36147d86c8
Ivan Solovev <ivan.solovev@qt.io>
2021-08-27 15:36:58 +0200

    Fix QTextCodec::canEncode() for ICU codec
    QTextCodec::canEncode() relies on the number of invalid characters
    to determine if the encoding is possible or not.
    
    By default the ICU fromUnicode converter method does not provide any
    ways to track the amount of failures. However it uses callbacks to
    report errors or replace unrecognized characters with substitute string.
    
    This patch introduces a custom callback for fromUnicode conversion.
    The callback just increases the invalid characters counter and then
    calls the default callback, which does its usual job.
    
    Pick-to: 6.2
    Task-number: QTBUG-83081
    Change-Id: Ic2c10285777128a25668551db2a0de4a1b9815a8
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- tests/auto/core5/codecs/qtextcodec/tst_qtextcodec.cpp
+++ tests/auto/core5/codecs/qtextcodec/tst_qtextcodec.cpp
@@ -108,6 +108,9 @@ private slots:
 
     void nullInputZeroOrNegativLength_data();
     void nullInputZeroOrNegativLength();
+
+    void canEncode();
+    void canEncode_data();
 };
 
 void tst_QTextCodec::toUnicode_data()
@@ -2687,6 +2690,61 @@ void tst_QTextCodec::nullInputZeroOrNegativLength()
     // zero, negative length
     QCOMPARE(codec->fromUnicode(QString("abc").constData(), 0), QByteArray());
     QCOMPARE(codec->fromUnicode(QString("abc").constData(), -1), QByteArray());
+}
+
+void tst_QTextCodec::canEncode()
+{
+    QFETCH(QString, codecName);
+    QFETCH(QString, inputString);
+    QFETCH(QByteArray, expectedData);
+    QFETCH(bool, canEncode);
+
+    QTextCodec *codec = QTextCodec::codecForName(codecName.toLatin1());
+    QVERIFY(codec != nullptr);
+
+    QCOMPARE(codec->canEncode(inputString), canEncode);
+    QByteArray encoded = codec->fromUnicode(inputString);
+    QCOMPARE(encoded, expectedData);
+}
+
+void tst_QTextCodec::canEncode_data()
+{
+    QTest::addColumn<QString>("codecName");
+    QTest::addColumn<QString>("inputString");
+    QTest::addColumn<QByteArray>("expectedData");
+    QTest::addColumn<bool>("canEncode");
+
+    QTest::newRow("English ISO-8859-1") << "ISO-8859-1" << "Hello World"
+                                        << QByteArray("Hello World") << true;
+    QTest::newRow("English big5") << "Big5" << "Hello World" << QByteArray("Hello World") << true;
+
+    QTest::newRow("Greek win1252")
+            << "Windows-1252"
+            << QString("\u03c0\u03bf\u03bb\u03cd\u03c4\u03c1\u03bf\u03c0\u03bf\u03bd")
+            << QByteArray("??????????") << false;
+    QTest::newRow("Greek win1253")
+            << "Windows-1253"
+            << QString("\u03c0\u03bf\u03bb\u03cd\u03c4\u03c1\u03bf\u03c0\u03bf\u03bd")
+            << QByteArray("\xF0\xEF\xEB\xFD\xF4\xF1\xEF\xF0\xEF\xED") << true;
+
+    QTest::newRow("Russian win1252")
+            << "Windows-1252" << QString("\u041f\u0440\u0438\u0432\u0435\u0442 \u043c\u0438\u0440")
+            << QByteArray("?????? ???") << false;
+    QTest::newRow("Russian win1251")
+            << "Windows-1251" << QString("\u041f\u0440\u0438\u0432\u0435\u0442 \u043c\u0438\u0440")
+            << QByteArray("\xCF\xF0\xE8\xE2\xE5\xF2 \xEC\xE8\xF0") << true;
+
+    QTest::newRow("English from ucs4")
+            << "ISO-8859-1" << QString("\u0048\u0065\u006c\u006c\u006f\u0021")
+            << QByteArray("Hello!") << true;
+
+    QTest::newRow("With null") << "ISO-8859-1" << QString::fromUcs4(U"Hello\u0000World", 11)
+                               << QByteArray("Hello\x00World", 11) << true;
+    QTest::newRow("With special chars")
+            << "ISO-8859-1" << QString("\u0001\u0002\u0003\u0008\u0009\u000a\u000b\u000d")
+            << QByteArray("\x01\x02\x03\b\t\n\x0B\r") << true;
+
+    QTest::newRow("Pencil icon") << "ISO-8859-1" << QString("\u270f") << QByteArray("?") << false;
 }
 
 QT_END_NAMESPACE

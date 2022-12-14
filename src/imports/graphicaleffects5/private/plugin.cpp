9d7bf51fc62e99540bc41b372ae00ccce063b4cc
Marc Mutz <marc.mutz@qt.io>
2022-12-14 16:32:37 +0100

    Port Qt5Compat.GraphicsEffects.private to qmltyperegistrar
    Fixes the missing plugins.qmltypes that makes qmllint complain about
    uses of the module.
    
    Fixes: QTBUG-105279
    Pick-to: 6.5
    Change-Id: I9e2fbe62631faf4481be66ad34c990b7f019bb8c
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/imports/graphicaleffects5/private/plugin.cpp
+++ src/imports/graphicaleffects5/private/plugin.cpp
@@ -1,35 +1,15 @@
+// Copyright (C) 2022 The Qt Company Ltd.
 // Copyright (C) 2016 Jolla Ltd, author: <gunnar.sletta@jollamobile.com>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include <QtQml/qqmlextensionplugin.h>
-#include <QtQml/qqml.h>
-#include <QtQml/qqmlengine.h>
-
-#include "qgfxshaderbuilder_p.h"
-#include "qgfxsourceproxy_p.h"
 
 QT_BEGIN_NAMESPACE
 
-static QObject *qgfxshaderbuilder_provider(QQmlEngine *, QJSEngine *)
-{
-    return new QGfxShaderBuilder();
-}
-
-class QtGraphicalEffectsPrivatePlugin : public QQmlExtensionPlugin
+class QtGraphicalEffectsPrivatePlugin : public QQmlEngineExtensionPlugin
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
-
-public:
-    QtGraphicalEffectsPrivatePlugin(QObject *parent = 0) : QQmlExtensionPlugin(parent) { }
-    void registerTypes(const char *uri) override
-    {
-        Q_ASSERT(QByteArray(uri) == QByteArrayLiteral("Qt5Compat.GraphicalEffects.private"));
-        qmlRegisterType<QGfxSourceProxy>(uri, 1, 0, "SourceProxy");
-        qmlRegisterSingletonType<QGfxShaderBuilder>(uri, 1, 0, "ShaderBuilder", qgfxshaderbuilder_provider);
-
-        qmlRegisterModule(uri, QT_VERSION_MAJOR, QT_VERSION_MINOR);
-    }
 };
 
 QT_END_NAMESPACE

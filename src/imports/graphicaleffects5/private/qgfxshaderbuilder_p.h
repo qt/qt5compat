cfcbf7334718a92370cbf33ea51c724f83c3080b
Tarja Sundqvist <tarja.sundqvist@qt.io>
2024-01-11 21:22:31 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 2251d63bec89eba3d943359c8e8d6ce42a240c05.
    
    Revert of commercial SPDX license identifiers is required for the
    Qt 6.5.x opensource releases, Qt 6.5.4 onwards.
    
    Change-Id: Ie2151e5720c7d8c8f86d8d3c7bdd7bf07171aed8
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/imports/graphicaleffects5/private/qgfxshaderbuilder_p.h
+++ src/imports/graphicaleffects5/private/qgfxshaderbuilder_p.h
@@ -1,6 +1,6 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // Copyright (C) 2016 Jolla Ltd, author: <gunnar.sletta@jollamobile.com>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QGFXSHADERBUILDER_P_H
 #define QGFXSHADERBUILDER_P_H

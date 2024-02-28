0fb52b344a14aee079da16fcb6f6b698f1103529
Marc Mutz <marc.mutz@qt.io>
2024-02-28 09:13:40 +0100

    tst_QStringRef: ensure QStringRef implicitly converts to QStringView
    It did, in Qt 5, but the test, if ever there was one, was lost when
    QStringRef moved out of QtCore.
    
    Pick-to: 6.6 6.5 6.2
    Change-Id: I3440074eb8678adaf317ff888c9f11d1a1e4c8ec
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    (cherry picked from commit 322b7f92e74cfd48e45a1634605b2d7a5935314c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/core5/text/qstringref/tst_qstringref.cpp
+++ tests/auto/core5/text/qstringref/tst_qstringref.cpp
@@ -3,12 +3,21 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include <QtTest/QtTest>
+
 #include <qstringlist.h>
+#include <QtCore/qstringview.h>
 #include <qvariant.h>
 
 #include <qlocale.h>
 #include <locale.h>
 #include <qstringref.h>
+
+#include <type_traits>
+
+static_assert(std::is_convertible_v<      QStringRef  , QStringView>);
+static_assert(std::is_convertible_v<      QStringRef& , QStringView>);
+static_assert(std::is_convertible_v<const QStringRef  , QStringView>);
+static_assert(std::is_convertible_v<const QStringRef&&, QStringView>);
 
 class tst_QStringRef : public QObject
 {

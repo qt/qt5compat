d6440a630ac262e7f583dcc11db56874f8e562b0
Marc Mutz <marc.mutz@qt.io>
2026-08-28 13:58:17 +0200

    Port qt5corecompat to auto-generated exports
    I don't know why d32f97e59301198b5b909872117ae0b0fbb7ab79 (6.8) didn't
    port the module. It's trivial, and helps with adding removed_api.cpp
    to this library.
    
    Do it now.
    
    Pick-to: 6.11 6.8
    Change-Id: I4a0e256a24a0a3984c59937ce25f19b1afa85ce7
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 6861c76bdfa8639a3ed9d42fd593da4367c09e03)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core5/qcore5global.h
+++ src/core5/qcore5global.h
@@ -7,19 +7,6 @@
 
 #include <QtCore/qglobal.h>
 #include <QtCore5Compat/qtcore5compat-config.h>
-
-QT_BEGIN_NAMESPACE
-
-#ifndef QT_STATIC
-#    if defined(QT_BUILD_CORE5COMPAT_LIB)
-#        define Q_CORE5COMPAT_EXPORT Q_DECL_EXPORT
-#    else
-#        define Q_CORE5COMPAT_EXPORT Q_DECL_IMPORT
-#    endif
-#else
-#    define Q_CORE5COMPAT_EXPORT
-#endif
-
-QT_END_NAMESPACE
+#include <QtCore5Compat/qtcore5compatexports.h>
 
 #endif // QCORE5GLOBAL_H

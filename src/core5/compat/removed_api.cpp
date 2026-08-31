19c46aa26ac83147fd2efe207cf382f209a96ba1
Ivan Solovev <ivan.solovev@qt.io>
2026-08-31 12:24:37 +0200

    core5: add removed_api.cpp
    Change-Id: I358ce52eaf3a5cce327c0f0f9ed19dd3ad328bcf
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit e613390300a0273ee468208f0ecba4f3ee46ff08)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/core5/compat/removed_api.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
+#define QT_CORE5COMPAT_BUILD_REMOVED_API
+
+#include "qcore5global.h"
+
+QT_USE_NAMESPACE
+
+#if QT_CORE5COMPAT_REMOVED_SINCE(6, 12)
+
+// #include "qotherheader.h"
+// // implement removed functions from qotherheader.h
+// order sections alphabetically to reduce chances of merge conflicts
+
+#endif // QT_CORE5COMPAT_REMOVED_SINCE(6, 12)

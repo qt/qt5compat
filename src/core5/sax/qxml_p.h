1190f3b369cddde22edf0d5637fcf38cde9874e0
Ivan Solovev <ivan.solovev@qt.io>
2026-09-25 10:33:21 +0200

    QXmlSimpleReader: Extract Methods Private::parse{Element,Content}Frame()
    These new helpers do the actual parsing without stack unwinding, using
    the provided parser state.
    
    For now these functions still have recursion - they call parseElement()
    and parseContent() on certain code paths.
    
    This change is created to factor out the tryUnwindStack() and the empty
    stack handling out of the actual parsing, simplifying the actual
    recursion fix.
    
    Task-number: QTBUG-149171
    Change-Id: Ib38ab1bb40575d5e4e1801d54cd2080d382fd134
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit de2eda7048492fef213d3b45ca15af20f2eb2b4b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit cc680e7de2887b6594b8ad0069e922b3323e65dc)

--- src/core5/sax/qxml_p.h
+++ src/core5/sax/qxml_p.h
@@ -223,6 +223,14 @@ private:
 
     bool parseBeginOrContinue(int state, bool incremental);
 
+    enum class FrameParseResult
+    {
+        Done,
+        Failed,
+    };
+    FrameParseResult parseElementFrame(int &state);
+    FrameParseResult parseContentFrame(int &state);
+
     bool parseProlog();
     bool parseElement();
     bool processElementEmptyTag();
